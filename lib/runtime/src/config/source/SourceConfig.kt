3581a333af8eb1b34f0a9dcc7e149b53ec05ea98
Martynas Petuška <martynas@petuska.dev>
2026-09-05 12:12:08 +0100

    feat: Add hero option to source configs

--- lib/runtime/src/config/source/SourceConfig.kt
+++ lib/runtime/src/config/source/SourceConfig.kt
@@ -1,12 +1,21 @@
 package dev.petuska.spond.sync.runtime.config.source
 
-import io.ktor.http.*
+import io.ktor.http.Url
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 sealed interface SourceConfig {
-  @Serializable @SerialName("BVA") data class BVA(val url: Url) : SourceConfig
+  /**
+   * URL to hero png to use for events created from this source.
+   */
+  val hero: Url?
 
-  @Serializable @SerialName("NVL") data class NVL(val url: Url) : SourceConfig
+  @Serializable
+  @SerialName("BVA")
+  data class BVA(val url: Url, override val hero: Url? = null) : SourceConfig
+
+  @Serializable
+  @SerialName("NVL")
+  data class NVL(val url: Url, override val hero: Url? = null) : SourceConfig
 }
